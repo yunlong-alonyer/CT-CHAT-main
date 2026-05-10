f9fbbff1ffcdbdd7aecdcae821b1a87227cb73e5
zlonyer <2091055152@qq.com>
2026-05-10 11:29:15 +0800

    预训练适配器代码调试

--- llava/train/train.py
+++ llava/train/train.py
@@ -1032,6 +1032,15 @@ def train(attn_implementation=None):
                 cache_dir=training_args.cache_dir,
                 **bnb_model_from_pretrained_args
             )
+        elif 'qwen' in model_args.model_name_or_path.lower():
+            from llava.model.language_model.llava_qwen import LlavaQwenForCausalLM
+            model = LlavaQwenForCausalLM.from_pretrained(
+                model_args.model_name_or_path,
+                cache_dir=training_args.cache_dir,
+                attn_implementation=attn_implementation,
+                torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
+                **bnb_model_from_pretrained_args
+            )
         else:
             model = LlavaLlamaForCausalLM.from_pretrained(
                 model_args.model_name_or_path,
