54168a1ef73da81fa1b50fe4ebafbec3b9ed60d6
zlonyer <2091055152@qq.com>
2026-05-11 14:20:15 +0800

    适配器LORA代码调试

--- finetune_qwen.sh
+++ finetune_qwen.sh
@@ -23,7 +23,6 @@ deepspeed --num_gpus 8 llava/train/train_mem.py \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
-    --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 500 \
     --save_total_limit 2 \
